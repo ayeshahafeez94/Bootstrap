613cca04ac8290418eb7d356327f3047fce8e5ba
Ayesha Hafeez <ayesha.hafeez@accenture.com>
2018-03-26 14:18:47 +0400

    Loaded properties into application

--- app/src/main/java/training/acn/com/bootstrap/MainActivity.java
+++ app/src/main/java/training/acn/com/bootstrap/MainActivity.java
@@ -1,13 +1,31 @@
 package training.acn.com.bootstrap;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.util.Properties;
+
+import training.acn.com.bootstrap.utils.AssetsPropertyReader;
 
 public class MainActivity extends AppCompatActivity {
+
+    private AssetsPropertyReader mAssetsPropertyReader;
+    private Context mContext;
+    private Properties mProperties;
+    String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mContext=this;
+        mAssetsPropertyReader = new AssetsPropertyReader(mContext);
+        mProperties=mAssetsPropertyReader.getProperties("application.properties");
+        Toast.makeText(mContext,mProperties.getProperty("BLOCK_COLOR"),Toast.LENGTH_LONG).show();
+        Log.v(TAG,"Block color "+mProperties.getProperty("BLOCK_COLOR"));
+        Log.v(TAG,"NO_OF_LIST_ITEMS "+mProperties.getProperty("NO_OF_LIST_ITEMS"));
     }
 }
