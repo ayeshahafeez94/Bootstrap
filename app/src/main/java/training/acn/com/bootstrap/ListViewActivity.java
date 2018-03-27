bf6b51e80501d664b20c06e8728270e142dd8466
afnan.khaleel <afnan.khaleel@accenture.com>
2018-03-27 15:49:30 +0400

    ListView files with error.

--- app/src/main/java/training/acn/com/bootstrap/ListViewActivity.java
+++ app/src/main/java/training/acn/com/bootstrap/ListViewActivity.java
@@ -1,13 +1,31 @@
 package training.acn.com.bootstrap;
 
+import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.LinearLayout;
+import android.widget.ListView;
 
-public class ListViewActivity extends AppCompatActivity {
+public class ListViewActivity extends Activity {
+    private ListView mListView ;
+
+    int icons[]= {R.drawable.robot1, R.drawable.robot1};
+    String name[] = {"hamesh", "leo"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_view);
+        mListView = (ListView) findViewById(R.id.listView);
+
+
+
+        ListViewAdpater mListViewAdapter = new ListViewAdpater(getApplicationContext(),name,icons);
+        mListView.setAdapter(mListViewAdapter);
+
+
+
+
     }
 }
